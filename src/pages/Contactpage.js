3750a8122692aa4bdb1c2f9d6ceede355c8094aa
olubiyi95 <82479253+olubiyi95@users.noreply.github.com>
2022-10-18 10:33:23 +0100

    Final fixes for landing page

--- src/pages/Contactpage.js
+++ src/pages/Contactpage.js
@@ -5,6 +5,19 @@ const Contactpage = () => {
   return (
     <div>
       <Navbar/>
+      <h1>Hello</h1>
+      <div className='contactpage'>
+        <div className="container text-light">
+          <div className="row">
+            <div className="col">
+              Column
+            </div>
+            <div className="col">
+              Column
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
